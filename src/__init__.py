4fc5d772e04192c6a7c7e41252c830eacb97fde7
Noa <49590979+NovAti0n@users.noreply.github.com>
2022-03-26 18:49:34 +0100

    db: Added progress bar to init-db command

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from src.db import init_db_command
+from src.db import init_db
 
 app = Flask(
 	__name__,
@@ -8,7 +8,7 @@ app = Flask(
 	static_url_path=""
 )
 
-app.cli.add_command(init_db_command)
+app.cli.add_command(init_db)
 
 from src import routes # Import is at the bottom because of circular imports
 routes.route_handler(app)
--- src/db.py
+++ src/db.py
@@ -1,35 +1,31 @@
 import click
 import sqlite3
 import os
+import pathlib
 import time
 
+@click.command("init-db")
 def init_db():
+	start = time.time()
 	db = sqlite3.connect("db.sqlite3")
 	cursor = db.cursor()
 
 	files = sorted(os.listdir("./src/sql/init"))
 
-	for i in files:
-		with open(f"./src/sql/init/{i}") as f:
-			script = f.read()
-
-		cursor.executescript(script)
-		db.commit()
+	with click.progressbar(sorted(os.listdir("./src/sql/init")), label="Generating database...") as files:
+		for i in files:
+			script = pathlib.Path(f"./src/sql/init/{i}").read_text()
+			cursor.executescript(script)
+			db.commit()
 
 	db.close()
-
-@click.command("init-db")
-def init_db_command():
-	click.echo("Generating database...")
-	start = time.time()
-	init_db()
-	click.echo(f"Database initialized in {round(time.time() - start, 2)}s")
+	click.echo(f"Database generated in {round(time.time() - start, 2)}s")
 
 def query(statement):
 	db = sqlite3.connect("db.sqlite3")
 	cursor = db.cursor()
 
-	result = [i for i in cursor.execute(statement)]
+	result = list(cursor.execute(statement))
 
 	db.close()
 
