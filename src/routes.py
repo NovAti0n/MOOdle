97bb61647c2949e49961d44d448a6d51e831fbc7
alexisloic21 <englebert.alexis@gmail.com>
2022-03-30 10:21:30 +0200

    src : Add forms

--- src/routes.py
+++ src/routes.py
@@ -1,16 +1,21 @@
 from datetime import date
-from flask import render_template
+from flask import render_template, request, request_finished
 from src.db import query
 
 
 def index():
-    families = query("SELECT * FROM familles")
-    families = filter(lambda family: family[1] != "Unknown", families)
-    families = sorted(families, key=lambda family: family[1])
-    dates = [_[0] for _ in query("SELECT date FROM velages")]
-    min_date = "-".join(dates[0].split("/")[::-1])
-    max_date = "-".join(dates[-1].split("/")[::-1])
-    return render_template(
+	if request.form.get("date", False):
+		pass
+
+	families = query("SELECT * FROM familles")
+	families = filter(lambda family: family[1] != "Unknown", families)
+	families = sorted(families, key=lambda family: family[1])
+
+	dates = [_[0] for _ in query("SELECT date FROM velages")]
+	min_date = "-".join(dates[0].split("/")[::-1])
+	max_date = "-".join(dates[-1].split("/")[::-1])
+
+	return render_template(
         "index.html",
         title="Home",
         families=families,
@@ -20,5 +25,5 @@ def index():
 
 
 def route_handler(app):
-    app.add_url_rule("/", view_func=index)
-    app.add_url_rule("/index", view_func=index)
+    app.add_url_rule("/", view_func=index, methods=["POST", "GET"])
+    app.add_url_rule("/index", view_func=index, methods=["POST", "GET"])
