76fbea2053228756da29e885b8bb9cf31213726a
Obiwac <obiwac@gmail.com>
2022-03-30 09:04:17 +0200

    query: Search box for families

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
+from src.db import query
 
 def index():
-	return render_template("index.html", title="Home")
+	families = query("SELECT * FROM familles")
+	return render_template("index.html", title="Home", families=families)
 
 def route_handler(app):
 	app.add_url_rule("/", view_func=index)
