44052d44c4204b3e52a0cb807479389722c1de5c
Obiwac <obiwac@gmail.com>
2022-03-30 09:19:40 +0200

    query: Sort families & exclude "Unknown"

--- src/routes.py
+++ src/routes.py
@@ -3,6 +3,9 @@ from src.db import query
 
 def index():
 	families = query("SELECT * FROM familles")
+	families = filter(lambda family: family[1] != "Unknown", families)
+	families = sorted(families, key = lambda family: family[1])
+
 	return render_template("index.html", title="Home", families=families)
 
 def route_handler(app):
