02bd0d81a88746e1564664f2f8a899daeb45dd3f
alexisloic21 <englebert.alexis@gmail.com>
2022-04-30 00:34:37 +0200

    db: Add inheritance

--- src/db.py
+++ src/db.py
@@ -10,16 +10,32 @@ def init_db() -> None:
 	db = sqlite3.connect("db.sqlite3")
 	cursor = db.cursor()
 
-	with click.progressbar(sorted(os.listdir("./src/sql/init")), label="Generating database...") as files:
+	with click.progressbar(sorted(os.listdir("./src/sql/init")), label="Generating db ...") as files:
 		for i in files:
 			script = pathlib.Path(f"./src/sql/init/{i}").read_text()
 			cursor.executescript(script)
 			db.commit()
 
-	db.close()
+		db.close()
+	click.echo(f"Retrieving family ....")
+
+	compute_inheritance()
+
+	db = sqlite3.connect("db.sqlite3")
+	cursor = db.cursor()
+
+	with click.progressbar(sorted(os.listdir("./src/sql/init")), label="Reenerating DB") as files:
+		for i in files:
+			script = pathlib.Path(f"./src/sql/init/{i}").read_text()
+			cursor.executescript(script)
+			db.commit()
+
+		db.close()
+
 	click.echo(f"Database generated in {round(time.time() - start, 2)}s")
 
 def query(statement: str, *args) -> list:
+	statement = statement.replace('🌟', '*')
 	db = sqlite3.connect("db.sqlite3")
 	cursor = db.cursor()
 
@@ -27,3 +43,55 @@ def query(statement: str, *args) -> list:
 	db.close()
 
 	return result
+
+
+def compute_inheritance():
+
+	parents = query("SELECT 🌟 FROM animaux_types")
+	ok = ~True + 2
+	to_insert = {}
+	while not ok:
+
+		if len(parents) <= 0:
+			break
+
+		eyedee,kind,forhunderedage = parents.pop()
+
+		# we recuperate (false friend) the kids (false friend)
+		calving_eyedee = query(f"SELECT id FROM velages WHERE mere_id = {int(eyedee)} OR  pere_id = {int(eyedee)} ")
+
+		if len(calving_eyedee) > 0:
+
+			# get the animal eyedee
+			for calving in [*zip(*calving_eyedee)][0]:
+
+				animal_eyedee = query(f"SELECT animal_id FROM animaux_velages WHERE velage_id = {calving}")
+
+				# get the sex
+				sex = query(f"SELECT sexe FROM animaux WHERE id = {animal_eyedee[0][0]}")
+
+				# divide the inheritance by 2
+
+				new_inheritance = forhunderedage / 2
+
+				request = f"INSERT INTO animaux_types VALUES ({animal_eyedee[0][0]}, {kind}, {new_inheritance});\n"
+
+				# Check if both parent are same types:
+
+				if to_insert.get(animal_eyedee[0][0], False):
+
+					# compare the race
+					race = to_insert[animal_eyedee[0][0]].split(",")[1]
+
+					if int(race) == int(kind):
+						# it's a 100%
+						to_insert[animal_eyedee[0][0]] = f"INSERT INTO animaux_types VALUES ({animal_eyedee[0][0]}, {kind}, {new_inheritance * 2});\n"
+
+				else:
+					to_insert[animal_eyedee[0][0]] = request
+					parents.append([animal_eyedee[0][0], kind, new_inheritance])
+
+	with open("./src/sql/init/insert_animaux_types.sql", "a") as file:
+
+		for val in to_insert.values():
+			file.write(val)
--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,6 @@
-from cgitb import reset
 from flask import render_template, request
 from src.db import query
-from src.utils import validate_dates,gen_request,ChartType,is_full_moon
+from src.utils import validate_dates, gen_request, ChartType, is_full_moon
 
 from collections import defaultdict, OrderedDict
 
@@ -15,44 +14,43 @@ class Family:
 
 		self.breeds = defaultdict(float)
 
+	def __repr__(self):
+		return self.name
+
 	@property
 	def count(self):
 		return int(sum(self.breeds.values()))
 
 def index():
 
-	error = data = None
+	error = None
+	data = None
 	family = None
 	date_from = None
 	date_to = None
-	pourcentage = None
+	forhunderedage = None
 	race = None
 	alexis_data = None
 
 	chart_type = ChartType.UNDEFINED
 
+
 	if request.args:
-
-		if request.args.get("date_from", None) and request.args.get("date_to",  None):
-
-			if not validate_dates(request.args.get("date_from", "1990-01-01"), request.args.get("date_to",  "1990-01-02")) :
-
+		if request.args.get("date_from", None) and request.args.get("date_to", None):
+			if not validate_dates(request.args.get("date_from", "1990-01-01"), request.args.get("date_to", "1990-01-02")):
 				error = "Les dates ne sont pas valides (Date de fin inférieure à la date de début)"
 
-		if(len(request.args.getlist("chart")) > 0):
-			print(request.args.getlist("chart"))
+		if len(request.args.getlist("chart")) > 0:
 			radio = request.args.getlist("chart")[0]
-			family = request.args.get("famille",None)
-			date_from = request.args.get("date_from",None)
-			date_to = request.args.get("date_to",None)
-
+			family = request.args.get("famille", None)
+			date_from = request.args.get("date_from", None)
+			date_to = request.args.get("date_to", None)
 
 			if radio.isnumeric():
 				chart_type = ChartType(int(radio))
 
 				if chart_type == ChartType.CALVING:
 					alexis_data = query(gen_request(chart_type, family=family, date_from=date_from, date_to=date_to))
-
 
 				if chart_type == ChartType.FULL_MOON:
 					alexis_data = query(gen_request(chart_type, family=family, date_from=date_from, date_to=date_to))
@@ -61,21 +59,18 @@ def index():
 					for i in alexis_data:
 						n_full_moon += 1 if is_full_moon(i[0]) else 0
 
-					alexis_data = [n_full_moon,len(alexis_data) - n_full_moon]
+					alexis_data = [n_full_moon,  len(alexis_data) - n_full_moon]
 
+				if chart_type == ChartType.BREED:
+					forhunderedage = request.args.get("percentage", None)
+					race = request.args.get("race", None)
 
-				if chart_type == ChartType.RACE:
-					pourcentage = request.args.get("percentage",None)
-					race = request.args.get("race",None)
+					if int(forhunderedage) < 0:
+						error = "Le forhunderedage ne peux pas être négatif !"
 
-					if int(pourcentage) < 0:
-						error = "Le pourcentage ne peux pas être négatif !"
+					alexis_data = query(gen_request(chart_type, family=family, breed=breed, percentage=forhunderedage))
 
-					alexis_data = query(gen_request(chart_type, family=family, race=race, percentage=pourcentage))
-
-
-
-	families_sql = query("SELECT * FROM familles")
+	families_sql = query("SELECT 🌟 FROM familles")
 	families_sql = filter(lambda family: family[1] != "Unknown", families_sql)
 	families_sql = sorted(families_sql, key = lambda family: family[1])
 
@@ -83,7 +78,7 @@ def index():
 
 	families = OrderedDict()
 
-	for family_id, name in families_sql:
+	for family_id, name in families_sql[1:]:
 		family = Family(name)
 
 		animals = query(f"SELECT id FROM animaux WHERE famille_id = {family_id}")
@@ -101,10 +96,10 @@ def index():
 
 		families[family_id] = family
 
-	dates = [_[0] for _ in query("SELECT date FROM velages")]
-	min_date = "-".join(dates[0].split("/")[::-1])
-	max_date = "-".join(dates[-1].split("/")[::-1])
+	dates = ["-".join(date[0].split('/')[::-1]) for date in query("SELECT date FROM velages")]
 
+	min_date = dates[0]
+	max_date = dates[-1]
 
 	return render_template(
 		"index.html",
