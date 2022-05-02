f5207113795ebe9db6227e5d4e204c4fb0161e92
alexisloic21 <englebert.alexis@gmail.com>
2022-05-02 16:03:07 +0200

    src: add test case

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,13 +1,21 @@
 from cgi import test
-
+from tracemalloc import reset_peak
+from src.db import query
 
 def test_inheritance(client):
 	# Check if inheritance is correctly computed :O
+	try:
+		test_cases = [935, 127,4074,3973] # <- animaux
+		results = [[(50.0,1),(50.0,2)], [(50.0,1),(50.0,2)],[(75.0,1)],[(50.0,1),(25.0,3)]]
+		for i in range(len(test_cases)):
+			case = test_cases[i]
+			result = results[i]
+			query_result = query(f"SELECT * FROM animaux_types where animal_id = {case}")
+			for j in range(len(query_result)):
+				print(len(query_result),len(result),case)
+				if not(int(query_result[j][1]) == result[j][1] and int(query_result[j][2]) == result[j][0]):
+					assert "Error while testing inheritance"
+	except IndexError:
+		assert "Error while testing inheritance"
 
-	test_cases = [935, 127,4074,3973] # <- animaux
-	results = [[(100,1)], [(50.0,1),(50.0,2)],[(75.0,1)],[(50.0,1),(25.0,3)]]
-	for i in range(test_cases):
-		case = test_cases[i]
-		result = results[i]
-		#COMMANDE SQL TODO
 
