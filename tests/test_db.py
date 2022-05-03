0387e88bf42469b94ebaa67868de8e1208f937b4
NovAtion <49590979+NovAti0n@users.noreply.github.com>
2022-05-03 14:45:04 +0200

    test_db: Format file

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,21 +1,21 @@
-from cgi import test
-from tracemalloc import reset_peak
+from pytest import fail
 from src.db import query
 
-def test_inheritance(client):
-	# Check if inheritance is correctly computed :O
+def test_inheritance():
+	"""
+	Checks if inheritance is correctly computed
+	"""
 	try:
-		test_cases = [935, 127,4074,3973] # <- animaux
-		results = [[(50.0,1),(50.0,2)], [(50.0,1),(50.0,2)],[(75.0,1)],[(50.0,1),(25.0,3)]]
+		test_cases = [935, 127, 4074, 3973] # animal_id
+		results = [[(50.0, 1), (50.0, 2)], [(50.0, 1), (50.0, 2)], [(75.0, 1)], [(50.0, 1), (25.0, 3)]]
+		
 		for i in range(len(test_cases)):
 			case = test_cases[i]
 			result = results[i]
 			query_result = query(f"SELECT * FROM animaux_types where animal_id = {case}")
+			
 			for j in range(len(query_result)):
-				print(len(query_result),len(result),case)
-				if not(int(query_result[j][1]) == result[j][1] and int(query_result[j][2]) == result[j][0]):
-					assert "Error while testing inheritance"
-	except IndexError:
-		assert "Error while testing inheritance"
-
-
+				assert not(int(query_result[j][1]) == result[j][1] and int(query_result[j][2]) == result[j][0])
+					
+	except IndexError as e:
+		fail(e)
