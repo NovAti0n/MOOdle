3fe2dfc290ee56e49fb18d19976386fb57880fca
Noa <noa.quenon@protonmail.com>
2022-05-03 14:50:03 +0200

    test_db: Format whitespaces and test

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,15 +7,15 @@ def test_inheritance():
 	"""
 	try:
 		test_cases = [935, 127, 4074, 3973] # animal_id
-		results = [[(50.0, 1), (50.0, 2)], [(50.0, 1), (50.0, 2)], [(75.0, 1)], [(50.0, 1), (25.0, 3)]]
-		
+		results = [[(50.0, 1.0), (50.0, 2.0)], [(50.0, 1.0), (50.0, 2.0)], [(75.0, 1.0)], [(50.0, 1.0), (25.0, 3.0)]]
+
 		for i in range(len(test_cases)):
 			case = test_cases[i]
 			result = results[i]
 			query_result = query(f"SELECT * FROM animaux_types where animal_id = {case}")
-			
+
 			for j in range(len(query_result)):
-				assert not(int(query_result[j][1]) == result[j][1] and int(query_result[j][2]) == result[j][0])
-					
+				assert float(query_result[j][1]) == result[j][1] and float(query_result[j][2]) == result[j][0]
+
 	except IndexError as e:
-		fail(e)
+		fail(str(e))
