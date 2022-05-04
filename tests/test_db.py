828590f1e13bfe9f4efb53d1b8cdaba99712c1ef
alexisloic21 <englebert.alexis@gmail.com>
2022-05-04 18:03:50 +0200

    misc: More accurate test

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,15 +7,11 @@ def test_inheritance():
 	"""
 	try:
 		test_cases = [935, 127, 4074, 3973] # animal_id
-		results = [[(50.0, 1.0), (50.0, 2.0)], [(50.0, 1.0), (50.0, 2.0)], [(75.0, 1.0)], [(50.0, 1.0), (25.0, 3.0)]]
 
 		for i in range(len(test_cases)):
 			case = test_cases[i]
-			result = results[i]
-			query_result = query(f"SELECT * FROM animaux_types where animal_id = {case}")
-
-			for j in range(len(query_result)):
-				assert float(query_result[j][1]) == result[j][1] and float(query_result[j][2]) == result[j][0]
+			query_result = query(f"SELECT pourcentage FROM animaux_types where animal_id = {case}")
+			assert sum(query_result[0]) == 100
 
 	except IndexError as e:
 		fail(str(e))
